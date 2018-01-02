4540902c782a3b489cde17db5c5d043064b680e8
Andre Chaves <andrechavesg@gmail.com>
2018-01-02 09:14:33 -0200

    capitulo8

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -4,12 +4,13 @@ from perfis.models import Perfil
 from django.shortcuts import redirect
 
 def index(request):
-	return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
+	return render(request, 'index.html', { 'perfis' : Perfil.objects.all(), 
+		'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
 	
 	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfil.html', { "perfil" : perfil })
+	return render(request, 'perfil.html', { "perfil" : perfil , 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
 
