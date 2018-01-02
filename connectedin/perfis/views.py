252ca0be13311c531e83acf510bf48addccb8cc7
Andre Chaves <andrechavesg@gmail.com>
2018-01-02 09:18:18 -0200

    capitulo12

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from perfis.models import Perfil, Convite
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
 	return render(request, 'index.html', { 'perfis' : Perfil.objects.all(), 
 		'perfil_logado' : get_perfil_logado(request)})
 
+@login_required
 def exibir(request, perfil_id):
 	
 	perfil = Perfil.objects.get(id=perfil_id)
@@ -15,6 +18,7 @@ def exibir(request, perfil_id):
 	return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 
 		'ja_eh_contato' : ja_eh_contato})
 
+@login_required
 def convidar(request, perfil_id):
 
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
@@ -23,9 +27,11 @@ def convidar(request, perfil_id):
 
 	return redirect('index');
 
+@login_required
 def get_perfil_logado(request):
-	return Perfil.objects.get(id=1) 
+	return request.user.perfil 
 
+@login_required
 def aceitar(request, convite_id):
 	convite = Convite.objects.get(id=convite_id)
 	convite.aceitar()
--- connectedin/usuarios/urls.py
+++ connectedin/usuarios/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from views import RegistrarUsuarioView
 
 urlpatterns = patterns('',
-	url(r'^registrar/$', RegistrarUsuarioView.as_view(), name="registrar")
+	url(r'^registrar/$', RegistrarUsuarioView.as_view(), name="registrar"),
+	url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html'}, name='login'),
+	url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url':'/login/'}, name='logout')
 )
